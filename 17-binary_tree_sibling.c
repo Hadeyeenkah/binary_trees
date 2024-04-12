0ef6b926e3c7e06a990d183b497be69e28cd0a27
samuel Adeyinka <tboysammy@gmail.com>
2024-04-12 15:25:45 +0100

    binary

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,20 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - finds the sibling of a node
- * @node: pointer to the node to find the sibling
+ * main - Entry point
  *
- * Return: pointer to the sibling node
- * NULL if node is NULL
- * NULL if the parent is NULL
- * NULL if the node has no siblings
+ * Return: Always 0 (Success)
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+int main(void)
 {
-	if (!node || !node->parent)
-		return (NULL);
+	binary_tree_t *root;
+	binary_tree_t *sibling;
 
-	if (node == node->parent->left)
-		return (node->parent->right);
-	return (node->parent->left);
+	root = binary_tree_node(NULL, 98);
+	root->left = binary_tree_node(root, 12);
+	root->right = binary_tree_node(root, 128);
+	root->left->right = binary_tree_node(root->left, 54);
+	root->right->right = binary_tree_node(root->right, 402);
+	root->left->left = binary_tree_node(root->left, 10);
+	root->right->left = binary_tree_node(root->right, 110);
+	root->right->right->left = binary_tree_node(root->right->right, 200);
+	root->right->right->right = binary_tree_node(root->right->right, 512);
+
+	binary_tree_print(root);
+	sibling = binary_tree_sibling(root->left);
+	printf("Sibling of %d: %d\n", root->left->n, sibling->n);
+	sibling = binary_tree_sibling(root->right->left);
+	printf("Sibling of %d: %d\n", root->right->left->n, sibling->n);
+	sibling = binary_tree_sibling(root->left->right);
+	printf("Sibling of %d: %d\n", root->left->right->n, sibling->n);
+	sibling = binary_tree_sibling(root);
+	printf("Sibling of %d: %p\n", root->n, (void *)sibling);
+	return (0);
 }
